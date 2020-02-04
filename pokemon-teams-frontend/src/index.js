7bc25657f21fbb7c65fe53b88fcfdcaf48729ed3
Marija Stojanovic <marijastojanovic5@yahoo.com>
2020-02-04 16:58:54 -0500

    almost

--- pokemon-teams-frontend/src/index.js
+++ pokemon-teams-frontend/src/index.js
@@ -1,3 +1,33 @@
 const BASE_URL = "http://localhost:3000"
 const TRAINERS_URL = `${BASE_URL}/trainers`
 const POKEMONS_URL = `${BASE_URL}/pokemons`
+document.addEventListener("DOMContentLoaded",()=>{
+    console.log("connected")
+    fetchAllTrainers()
+
+})
+function fetchAllTrainers(){
+    fetch("http://localhost:3000/trainers")
+    .then(response =>response.json())
+    .then(trainerArray =>{
+        trainerArray.forEach(trainer =>buildTrainerCard(trainer))
+    })
+
+}
+function buildTrainerCard(trainer){ 
+    let page = document.querySelector('main')
+   let trainerCard = document.createElement('div')
+    trainerCard.className = "card" 
+    trainerCard.dataset.id = trainer.id 
+    page.appendChild(trainerCard)
+    pokeUl = document.createElement('ul')
+    trainer.pokemons.forEach(pokemon => { 
+        let pokemonLi = document.createElement('li')
+        pokemonLi.innerText = ` ${pokemon.nickname} (${pokemon.species}) `
+        pokeUl.appendChild(pokemonLi)
+    })
+    let trainerName = document.createElement('p')
+    trainerName.innerText = trainer.name
+    trainerCard.append(trainerName, pokeUl)
+}
+
